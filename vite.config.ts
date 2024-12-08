2e57a175e7300479986f84626bf7f8656e761f5b
Yakirbe <yaqir1@gmail.com>
2024-12-08 21:02:29 +0200

    Update dependencies and npm registry configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/tp5chat/'
+  base: '/tp5chat/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  }
 }); 
